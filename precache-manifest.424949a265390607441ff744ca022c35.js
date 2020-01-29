498e1cc5f5b2f2dc20a8adcdcd0cb97ff9f7af48
Vlad <vladik280699@gmail.com>
2020-01-29 22:18:52 +0200

    Updates

---
+++ precache-manifest.424949a265390607441ff744ca022c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2fb356691c3f01fe7236ccad420a2d",
+    "url": "/My-to-do-list/index.html"
+  },
+  {
+    "revision": "fd1a1c5d32e0c2bbecc5",
+    "url": "/My-to-do-list/static/css/main.eb6f8d93.chunk.css"
+  },
+  {
+    "revision": "696d1c23cd80f91439c0",
+    "url": "/My-to-do-list/static/js/2.88d32e52.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/My-to-do-list/static/js/2.88d32e52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd1a1c5d32e0c2bbecc5",
+    "url": "/My-to-do-list/static/js/main.9dd3e134.chunk.js"
+  },
+  {
+    "revision": "1b9d58e5e2ef26a911ee",
+    "url": "/My-to-do-list/static/js/runtime-main.7d33933f.js"
+  }
+]);
