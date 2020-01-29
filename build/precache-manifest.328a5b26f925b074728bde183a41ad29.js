e16a26da8c2798f6333e53665f08ba5ea734ba1a
Vlad <vladik280699@gmail.com>
2020-01-29 21:28:07 +0200

    Created build folder

---
+++ build/precache-manifest.328a5b26f925b074728bde183a41ad29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e718306218e98830d1d1f241947ed5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71424361ce0ca7fd934f",
+    "url": "/static/css/main.eb6f8d93.chunk.css"
+  },
+  {
+    "revision": "696d1c23cd80f91439c0",
+    "url": "/static/js/2.88d32e52.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.88d32e52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71424361ce0ca7fd934f",
+    "url": "/static/js/main.2828e373.chunk.js"
+  },
+  {
+    "revision": "4b59169021250af309b3",
+    "url": "/static/js/runtime-main.60eb9423.js"
+  }
+]);
